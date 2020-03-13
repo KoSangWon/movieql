bff9383015e474c787d6b2c7bd41ed05cf631b09
KoSangWon <jackoss@naver.com>
2020-03-13 19:24:30 +0900

    Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,32 +1,49 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
+        id: 0,
+        name: "Star wars - The new one",
+        score: 1
     },
     {
-        id: "1",
-        name: "KoSang",
-        age: 24,
-        gender: "male"
+        id: 1,
+        name: "Avengers - The new one",
+        score: 8
     },
     {
-        id: "2",
-        name: "Sangwon",
-        age: 25,
-        gender: "male"
+        id: 2,
+        name: "The Gotfather I",
+        score: 99
     },
     {
-        id: "3",
-        name: "Suzy",
-        age: 25,
-        gender: "female"
+        id: 3,
+        name: "Logan",
+        score: 2
     }
-]
+];
 
+export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
